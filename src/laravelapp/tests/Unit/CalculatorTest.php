e58d9c9fca0070e22c407c8095ea2637214772c8
KazuyaKato <soanevalcke@hotmail.co.jp>
2023-08-12 20:11:25 +0900

    Create CalculatorTest.php

---
+++ src/laravelapp/tests/Unit/CalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * コマンド実行する場合はプロジェクトのルートディレクトリ上で実行すること
+ * $ ./vendor/bin/phpunit ./tests/Unit/CalculatorTest.php
+ */
+class CalculatorTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
