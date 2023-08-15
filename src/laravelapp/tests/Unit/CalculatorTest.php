c8bae1fcc8d9abe42db65d0f09b32420fe9f7312
KazuyaKato <soanevalcke@hotmail.co.jp>
2023-08-15 09:05:18 +0900

    setUp実装

--- src/laravelapp/tests/Unit/CalculatorTest.php
+++ src/laravelapp/tests/Unit/CalculatorTest.php
@@ -12,6 +12,14 @@ use PHPUnit\Framework\TestCase;
  */
 class CalculatorTest extends TestCase
 {
+    private $target;
+
+    protected function setUp(): void
+    {
+        // Calculatorクラスのインスタンスを生成
+        $this->target = new Calculator();
+    }
+    
     /**
      * public function Multiply_3と4の乗算結果を取得する
      *
@@ -19,14 +27,11 @@ class CalculatorTest extends TestCase
      */
     public function testMultiply_3と4の乗算結果を取得する()
     {
-        // Calculatorクラスのインスタンスを生成
-        $target = new Calculator();
-
         // 想定する計算結果の用意 → 期待値
         $expected = 12;
 
         // 3 x 4の結果を multiply()に計算させる → 実測値
-        $actual = $target->multiply(3, 4);
+        $actual = $this->target->multiply(3, 4);
 
         // 想定する結果と multiply()の結果が等しいか検証する
         $this->assertSame($expected, $actual);
@@ -34,29 +39,26 @@ class CalculatorTest extends TestCase
 
     public function testMultiply_5と6の乗算結果を取得する()
     {
-        $target = new Calculator();
         $expected = 30;
-        $actual = $target->multiply(5, 6);
+        $actual = $this->target->multiply(5, 6);
 
         $this->assertSame($expected, $actual);
     }
 
     public function testDivide_3と2の除算結果を取得する()
     {
-        $target = new Calculator();
         $expected = 1.5;
-        $actual = $target->divide(3, 2);
+        $actual = $this->target->divide(3, 2);
 
         $this->assertSame($expected, $actual);
     }
 
     public function testDivide_整数を0除算する()
     {
-        $target = new Calculator();
         // 発生する例外クラス →　期待結果
         $this->expectException(Exception::class);
         // 例外が発生する処理
-        $actual = $target->divide(10, 0);
+        $actual = $this->target->divide(10, 0);
     }
 
     /**
@@ -66,14 +68,11 @@ class CalculatorTest extends TestCase
      */
     public function testSum_3と2の加算結果を取得する()
     {
-        // Calculatorクラスのインスタンスを生成
-        $target = new Calculator();
-
         // 想定する計算結果の用意 → 期待値
         $expected = 5;
 
         // 3 + 2の結果を`sum()`に計算させる →　実測値
-        $actual = $target->sum(3, 2);
+        $actual = $this->target->sum(3, 2);
 
         // 期待値と実測値を検証する
         $this->assertSame($expected, $actual);
@@ -86,14 +85,11 @@ class CalculatorTest extends TestCase
      */
     public function testSubtract_10と3の減算結果を取得する()
     {
-        // Calculatorクラスのインスタンスを生成
-        $target = new Calculator();
-
         // 想定する計算結果の用意　→　期待値
         $expected = 7;
 
         // 10 - 3の結果を`subtract()`に計算させる →　実測値
-        $actual = $target->subtract(10, 3);
+        $actual = $this->target->subtract(10, 3);
 
         // 期待値と実測値を検証する
         $this->assertSame($expected, $actual);
@@ -101,9 +97,8 @@ class CalculatorTest extends TestCase
 
     public function testCalcTriangleArea_底辺が3で高さが5の三角形の面積を取得する()
     {
-        $target = new Calculator();
         $expected = 7.5;
-        $actual = $target->calcTriangleArea(3, 5);
+        $actual = $this->target->calcTriangleArea(3, 5);
 
         $this->assertSame($expected, $actual);
     }
